f383e3ba5e19b1ce1131f7b6174d3e2c4eee9ca7
Ritik Anand Kumar <Anshritik1>
2026-04-15 22:51:06 +0530

    Initial commit

---
+++ bootjpaexample/src/main/java/com/jpa/test/BootjpaexampleApplication.java
@@ -0,0 +1,62 @@
+package com.jpa.test;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.jpa.test.dao.UserRepository;
+import com.jpa.test.entities.User;
+
+@SpringBootApplication
+public class BootjpaexampleApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(BootjpaexampleApplication.class, args);
+		UserRepository userRepository = context.getBean(UserRepository.class);
+		
+		
+//		User user = new User();
+//		user.setName("Ritik Anand");
+//		user.setCity("Patna");
+//		user.setStatus("i am java developer");
+//		
+//		User user1 = userRepository.save(user);
+//		System.out.println(user1);
+		
+//		create object of users
+		User user = new User();
+		user.setName("manish");
+		user.setCity("Delhi");;
+		user.setStatus("I am a fullstack developer.");
+		
+		User user2 = new User();
+		user2.setName("Riya");
+		user2.setCity("Patna");
+		user2.setStatus("I am a human resource enthusist.");
+		
+//		saving single users...
+//		User resultUser = userRepository.save(user2);
+//		System.out.println(resultUser);
+		List<User> users = List.of(user,user2);
+		userRepository.saveAll(users);
+		System.out.println("Saved users successfully...");		
+		
+		
+		
+//		Update the user of id 3
+	Optional <User> optional =	userRepository.findById(1);
+		
+	User user1 = optional.get();
+	System.out.println(user1);
+//	if(optional.isPresent()) {
+//	    User user1 = optional.get();
+//	    System.out.println(user1);
+//	} else {
+//	    System.out.println("User not found");
+//	}
+	}
+
+}
