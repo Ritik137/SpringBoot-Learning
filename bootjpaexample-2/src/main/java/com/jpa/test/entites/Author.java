5a2c2299e4a3a420257efd96be7e7c8113982fe5
Ritik Anand Kumar <anshritik1@gmail.com>
2026-05-08 22:54:07 +0530

    Practiced Spring Boot microservices, JPA, repositories, and JSON annotations

---
+++ bootjpaexample-2/src/main/java/com/jpa/test/entites/Author.java
@@ -0,0 +1,74 @@
+package com.jpa.test.entites;
+
+//import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Table;
+
+@Entity
+//@Table(name="authors")
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // ✅
+	private int authorId;
+	
+ 
+	private String firstName;
+	private String lastName;
+	private String language;
+	
+	@OneToOne(mappedBy = "author")
+	@JsonBackReference
+	private Book book;
+	  public Author() {
+
+	    }
+	
+	public int getAuthorId() {
+		return authorId;
+	}
+	
+	public void setAuthorId(int authorId) {
+		this.authorId=authorId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName=firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	
+}
--- bootjpaexample-2/src/main/java/com/jpa/test/entites/Book.java
+++ bootjpaexample-2/src/main/java/com/jpa/test/entites/Book.java
@@ -1,29 +1,37 @@
 package com.jpa.test.entites;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+//import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="books")
 public class Book {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="book_id")
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // ✅
+	@Column(name="book_id")
+	private int id;
 
     private String title;
-    private String author;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Author author;
 
     public Book() {
 
     }
 
-    public Book(int id, String title, String author) {
+    public Book(int id, String title, Author author) {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -47,11 +55,11 @@ public class Book {
     }
 
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
